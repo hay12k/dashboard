445c02ae6c9fa26b4a7c657c513e30cab3be3881
hay12k <hayaangroup93@gmail.com>
2023-03-23 18:40:06 +0300

    modified:   assets/css/style.css 	new file:   assets/images/pro.jpeg 	modified:   assets/js/script.js 	modified:   index.html

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+      
+      modeSwitch.addEventListener("click", () =>{
+        body.classList.toggle("dark");
+        if(body.classList.contains("dark")){
+          modeText.innerText = "Light Mode"
+        }else{
+          modeText.innerText = "Dark Mode"
+        }
+      });
+
+      toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+      });
