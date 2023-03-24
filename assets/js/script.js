e2398e4295b783671198037d9ec9de195d88adc6
hay12k <hayaangroup93@gmail.com>
2023-03-24 14:08:46 +0300

    modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,5 @@
 const body = document.querySelector("body")
-      // sidebar = body.querySelector(".sidebar"),
-      // toggle = body.querySelector(".toggle"),
-      // searchBtn = body.querySelector(".search-box"),
-      // modeSwitch = body.querySelector(".toggle-switch"),
-      // modeText = body.querySelector(".mode-text");
-      
-      // modeSwitch.addEventListener("click", () =>{
-      //   body.classList.toggle("dark");
-      //   if(body.classList.contains("dark")){
-      //     modeText.innerText = "Light Mode"
-      //   }else{
-      //     modeText.innerText = "Dark Mode"
-      //   }
-      // });
+     
       const themeToggler = document.querySelector(".theme-Toggler");
       themeToggler.addEventListener('click', () => {
         document.body.classList.toggle('dark');
